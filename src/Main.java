fb23a782b4fc11f480af8daf795940f4a4872b8d
briansenas <briansena23@hotmail.com>
2022-12-22 15:38:02 +0100

    Quick Prototype v1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 public class Main {
